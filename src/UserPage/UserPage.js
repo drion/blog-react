1874210356fe2a5fcf5f70dc4ceaecbb101d5cec
Borys Yudin <borysyudindm@gmail.com>
2018-09-02 18:04:31 +0300

    Add condition button

--- src/UserPage/UserPage.js
+++ src/UserPage/UserPage.js
@@ -3,17 +3,23 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 
 import userActions from "../_actions/user.actions";
 import postActions from "../_actions/post.actions";
-import { getShowUser, getAllUserPosts } from "../_reducers/";
+import { getShowUser, getAllUserPosts, getCurrentUser } from "../_reducers/";
 
 import PostsList from "../HomePage/PostsList";
 
 const styles = {
     root: {
-        maxWidth: 700,
+        maxWidth: 624,
         margin: "0 auto"
+    },
+    pageHeader: {
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center"
     }
 };
 
@@ -26,10 +32,17 @@ class UserPage extends React.Component {
     }
 
     render() {
-        const { showUser, posts, classes } = this.props;
+        const { showUser, posts, classes, currentUser } = this.props;
         return showUser.isLoading ? null : (
             <div className={classes.root}>
-                <h1>{showUser.username} posts</h1>
+                <div className={classes.pageHeader}>
+                    <h1>{showUser.username} posts</h1>
+                    {currentUser.id == this.props.match.params.id && (
+                        <Button variant="contained" className={classes.button}>
+                            Add post
+                        </Button>
+                    )}
+                </div>
                 {posts.length > 0 ? (
                     <PostsList posts={posts} />
                 ) : (
@@ -42,7 +55,8 @@ class UserPage extends React.Component {
 
 const mapStateToProps = state => ({
     showUser: getShowUser(state),
-    posts: getAllUserPosts(state)
+    posts: getAllUserPosts(state),
+    currentUser: getCurrentUser(state)
 });
 
 export default withStyles(styles)(
